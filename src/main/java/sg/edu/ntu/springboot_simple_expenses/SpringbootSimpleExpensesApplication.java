75928a44cb35d5e2172dc53a2a64b516fff1bafc
Soh <dexsjw@gmail.com>
2024-09-01 03:35:58 +0800

    initial commit

---
+++ src/main/java/sg/edu/ntu/springboot_simple_expenses/SpringbootSimpleExpensesApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.ntu.springboot_simple_expenses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSimpleExpensesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSimpleExpensesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sg/edu/ntu/springboot_simple_expenses/SpringbootSimpleExpensesApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.ntu.springboot_simple_expenses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSimpleExpensesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
